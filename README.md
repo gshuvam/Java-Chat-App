033dd67eb6f7c5bda804968c07f6084606df8d89
Shuvam Ghosh <shuvam1309@gmail.com>
2022-10-25 13:00:31 +0530

    Unzipped souce code zip file

--- README.md
+++ README.md
@@ -1,8 +1,49 @@
-# Java_chat_app
-Chat application made with java
+# Welcome to JAVA Chat App!
+<br/>
 
->The zip file contains all the information: 
->source code ;
->actual program;
->screenshots of programs;
->readme file;
+
+## Getting Started
+
+There are runnable .jar files named ClientServer.jar and ClientChatApp.jar. What we have to do is just double click those runnable files and they will run. Server requires a host name to host the server. The program will automatically detect the local ip address of the host machine and start the server. Otherwise, you can enter ip address manually.
+In the client app you need to provide an username and host IP address and our program will automatically connect it. It uses a secure algorithm to secure chats and help it from being stolen. 
+You can create multile clients and connect to the server. The app is completely portable from one device to another. It will run on any java supported machine.
+<br/>
+<br/>
+
+## Depencies:
+	The app only needs java installed in the computer to run! thats all!
+<br/>
+<br/>
+
+## Architecture
+	Client - Server
+<br/>
+<br/>
+
+## Security Functions
+
+* This app is secured by encryption.
+* It does not contain any harmful contains
+* We do not store your data
+* All chats are self-destructive. i.e After chat messages will be deleted
+* There is a logout function to help logging out from the app
+<br/>
+<br/>
+
+## TroubleShooting
+
+If you are unable to execute the java files then probably you have no executable permission for the jar file in your pc. In that case please grant permission.
+To do so in linux type "chmod +x "file name"" in terminal.
+<br/>
+
+	For further assistance please contact "shuvam1309@gmail.com"
+<br/>
+<br/>
+
+## Contact Info
+
+	Email: shuvam1309@gmail.com
+
+
+
+
---
+++ Source Codes/Client.java
@@ -0,0 +1,293 @@
+package com.shuvam.chatApp;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.text.DefaultCaret;
+
+public class Client extends JFrame implements ActionListener, KeyListener,
+        FocusListener {
+
+    // Extra variables
+    static String message = "";
+    static String userName = "";
+    static String iPAddress = null;
+
+    // Networking Variables
+    static Socket socket = null;
+    static PrintWriter writer = null;
+
+    // // Graphics Variables
+    static JTextArea msgRec = new JTextArea(100, 50);
+    static JTextArea msgSend = new JTextArea(100, 50);
+    JButton send = new JButton("Send");
+    JScrollPane pane2, pane1;
+
+    JMenuBar bar = new JMenuBar();
+
+    JMenu messanger = new JMenu("Messanger");
+    JMenuItem logOut = new JMenuItem("Log Out");
+
+    JMenu help = new JMenu("Help");
+    JMenuItem s_keys = new JMenuItem("Shortcut Keys");
+    JMenuItem about = new JMenuItem("about");
+
+    public Client() {
+        super("Java Client");
+        setBounds(0, 0, 407, 495);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLayout(null);
+
+        msgRec.setEditable(false);
+        msgRec.setBackground(Color.BLACK);
+        msgRec.setForeground(Color.WHITE);
+        msgRec.addFocusListener(this);
+        msgRec.setText("");
+
+        msgRec.setWrapStyleWord(true);
+        msgRec.setLineWrap(true);
+
+        pane2 = new JScrollPane(msgRec);
+        pane2.setBounds(0, 0, 400, 200);
+        pane2.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        add(pane2);
+
+        msgSend.setBackground(Color.LIGHT_GRAY);
+        msgSend.setForeground(Color.BLACK);
+        msgSend.setLineWrap(true);
+        msgSend.setWrapStyleWord(true);
+
+        msgSend.setText("Write Message here");
+        msgSend.addFocusListener(this);
+        msgSend.addKeyListener(this);
+
+        pane1 = new JScrollPane(msgSend);
+        pane1.setBounds(0, 200, 400, 200);
+        pane1.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        add(pane1);
+
+        send.setBounds(0, 400, 400, 40);
+        add(send);
+        send.addActionListener(this);
+
+        bar.add(messanger);
+        messanger.add(logOut);
+        logOut.addActionListener(this);
+
+        bar.add(help);
+        help.add(s_keys);
+        s_keys.addActionListener(this);
+        help.add(about);
+        about.addActionListener(this);
+
+        setJMenuBar(bar);
+
+        addWindowFocusListener(new WindowFocusListener() {
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+
+                if (!msgRec.getText().equals("")) {
+                    System.out.println("Yes Focus");
+
+                    writer.println("\t<<<" + userName + ": Focusing to Comunication Windo>>>");
+                    writer.flush();
+
+                }
+            }
+
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+                if (!msgRec.getText().equals("")) {
+                   writer.println("\t<<<" + userName + ": Ignoring to Comunication Window>>>");
+                   writer.flush();
+                }
+            }
+
+        });
+
+        if ((userName) != null) {
+            setVisible(true);
+        } else {
+            System.exit(0);
+        }
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO Auto-generated method stub
+
+        userName = JOptionPane.showInputDialog("User Name (Client)");
+        iPAddress = JOptionPane.showInputDialog("Enter Server IpAddress");
+
+        // swing thread
+        (new Thread(new Runnable() {
+            public void run() {
+                new Client();
+
+            }
+
+        })).start();
+
+        socket = new Socket(iPAddress, 8888);
+        msgRec.setText("Connected!");
+
+        // listening port thread
+        (new Thread(new Runnable() {
+            public void run() {
+
+                try {
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(socket.getInputStream()));
+
+                    String line = null;
+                    boolean testFlag = true;
+                    while ((line = reader.readLine()) != null) {
+                        msgRec.append("\n" + line);
+                        cursorUpdate();
+
+                        if (!reader.ready()) {
+                            testFlag = true;
+                        }
+                    }
+
+                } catch (IOException ee) {
+                    try {
+                        socket.close();
+                    } catch (IOException eee) {
+                        eee.printStackTrace();
+                    }
+                    ee.printStackTrace();
+                }
+            }
+        })).start();
+
+        try {
+            writer = new PrintWriter(socket.getOutputStream(), true);
+
+        } catch (IOException e) {
+            try {
+                socket.close();
+            } catch (IOException eee) {
+            }
+        }
+    }
+
+    // ActionEvents
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object scr = e.getSource();
+
+        if (scr == send) {
+            sendMessage();
+        } else if (scr == logOut) {
+
+            System.exit(0);
+
+        } else if (scr == s_keys) {
+
+            JOptionPane.showMessageDialog(this,
+                    "(shift+Enter) for new line while writing message"
+                            + "\n(ctrl+x) for quit");
+
+        } else if (scr == about) {
+            JOptionPane.showMessageDialog(this,
+                    "Messanger 1.0");
+        }
+    }
+
+    // / KeyBoardEvents
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        if ((e.getKeyCode() == KeyEvent.VK_ENTER) && e.isShiftDown()) {
+            msgSend.append("\n");
+
+        } else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            sendMessage();
+        } else if ((e.getKeyCode() == KeyEvent.VK_X) && e.isControlDown()) {
+            System.exit(0);
+        }
+    }
+
+    // FocusEvents
+    @Override
+    public void focusGained(FocusEvent e) {
+
+        if (e.getSource() == msgRec) {
+            if (!(msgRec.getText().equals("") || msgRec.getText().equals(
+                    "Connected!"))) {
+
+                writer.println("\t ***" + userName
+                        + ": reading......***");
+                writer.flush();
+            }
+        } else if (e.getSource() == msgSend) {
+            // Set Mesg sending area clear
+            if (msgSend.getText().equals("Write Message here")) {
+                msgSend.setText("");
+            } else {
+                writer.println("\t ***" + userName
+                        + ": typing......***");
+                writer.flush();
+            }
+
+        }
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+    }
+
+    private void sendMessage() {
+        writer.println(userName + " :" + msgSend.getText());
+
+        msgRec.append("\nMe: " + msgSend.getText());
+        writer.flush();
+        cursorUpdate();
+
+        msgSend.setText("");
+        msgSend.setCaretPosition(0);
+    }
+
+    private static void cursorUpdate() {
+        // Update cursor position
+        DefaultCaret caret = (DefaultCaret) msgRec.getCaret();
+        caret.setDot(msgRec.getDocument().getLength());
+
+        DefaultCaret caret2 = (DefaultCaret) msgSend.getCaret();
+        caret2.setDot(msgSend.getDocument().getLength());
+    }
+}
